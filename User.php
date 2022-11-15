98994024de5b6bb20bb6d78915cd38fd566a5969
akinyiliz <lizzieke@gmail.com>
2022-11-15 11:01:02 +0300

    add registration code

---
+++ User.php
@@ -0,0 +1,15 @@
+<?php
+class User {
+    private $fullname;
+    private $email;
+    private $password;
+    private $confirm_password;
+
+    public function validate(){
+        if(!isset($this.fullname)){
+
+        }
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+</head>
+<body>
+    <div class="document">
+        <nav>
+            <ul>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+        <main>
+            <h1>Shopping Application</h1>
+        </main>
+    </div>
+</body>
+</html>
